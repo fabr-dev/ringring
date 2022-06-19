fb5e21bae4d34ee5841116a503ca5039dd578c5e
fabiolabr.dev <fabiolabr.dev@gmail.com>
2022-06-19 13:24:31 -0700

    video is captured and displayed on screen

---
+++ videoCapture.js
@@ -0,0 +1,28 @@
+const cv = require('opencv4nodejs')
+
+async function wait(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, timeout)
+    })
+}
+
+async function capture() {
+    const videoCapture = new cv.VideoCapture(0); // 0 id of the video capture device, returns a matrix of the image
+    
+    videoCapture.set(cv.CAP_PROP_FRAME_WIDTH, 500);
+    videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT, 500);
+
+    const delay = 10;
+    let done = false;
+    while (!done) {
+        let frame = videoCapture.read().flip(1); // reads the image from the video capture every second
+        cv.imshow('video', frame);
+        const key = cv.waitKey(delay);
+        console.log(key)
+        done = key !== -1 && key === 32;
+    }   
+}
+
+capture();
