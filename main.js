81d58877ab37dbb5acb973d57d9f6d9e095e7791
Jahdiel Alvarez <jahdiel.alvarez@gmail.com>
2022-06-18 23:39:19 -0700

    Minor fix: double window creation

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
 const { app, BrowserWindow } = require("electron");
 
-const path = require('path')
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
 };
-
-app.whenReady().then(() => {
-  createWindow();
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
 
 app.whenReady().then(() => {
   createWindow();
@@ -29,4 +21,3 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
-
