11435606340a8ad9b3e6408a3583b15c937d9ea0
fabiolabr.dev <fabiolabr.dev@gmail.com>
2022-06-24 17:44:34 -0700

    changed while for setInterval so that main process doesnt get stuck in infinite loop

--- src/videoCapture/videoCapture.ts
+++ src/videoCapture/videoCapture.ts
@@ -15,11 +15,8 @@ export default async function capture(frameSender: FrameSender) {
 
   videoCapture.set(cv.CAP_PROP_FRAME_WIDTH, 500);
   videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT, 500);
-
-  const delay = 10;
-  let done = false;
-  while (!done) {
-    await wait(16);
+  
+  setInterval(() => {
     let frame = videoCapture.read().flip(1); // reads the image from the video capture every second
     frame = frame.resize(256, 256);
     frame =
@@ -27,7 +24,6 @@ export default async function capture(frameSender: FrameSender) {
         ? frame.cvtColor(cv.COLOR_GRAY2RGBA)
         : frame.cvtColor(cv.COLOR_BGR2RGBA);
     frameSender.sendFrame({ buf: frame.getData(), row: frame.rows, col: frame.cols });
-    const key = cv.waitKey(delay);
-    done = key !== -1 && key === 32;
-  }
+  }, 16);
+
 }
